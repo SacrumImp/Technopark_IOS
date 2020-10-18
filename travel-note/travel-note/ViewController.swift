8ec75c054abde16e9772611cb1a08d9edf15d46c
VladislavAlpeev <vlad.alpeev@mail.ru>
2020-10-18 03:02:51 +0300

    инициализация проекта

---
+++ travel-note/travel-note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-note
+//
+//  Created by Владислав Алпеев on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
