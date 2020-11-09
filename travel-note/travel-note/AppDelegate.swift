4d4dbd6e15f651c09e1308d724ab9b826e718479
VladislavAlpeev <vlad.alpeev@mail.ru>
2020-11-09 16:46:02 +0300

    исправлена версия в pod файле, api вынесен в ресурсы

--- travel-note/travel-note/AppDelegate.swift
+++ travel-note/travel-note/AppDelegate.swift
@@ -13,12 +13,16 @@ import GoogleMaps
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //    private var splashPresenter: SplashPresenter? = SplashPresenter()
+    
+    var dictOfAPIs = [String: Dictionary<String, String>]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        getAPIs()
+        
         FirebaseApp.configure()
         
-        GMSServices.provideAPIKey("AIzaSyAwvdiMqit4mnFfFKwp7Df0IT_RrSeHTH4")
+        GMSServices.provideAPIKey(dictOfAPIs["GMSServices"]?["key"] ?? "0")
         
         return true
     }
@@ -35,6 +39,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func getAPIs(){
+        guard let path = Bundle.main.path(forResource: "API", ofType: ".plist") else { return }
+        guard let dictionary = NSDictionary(contentsOfFile: path) else { return }
+        self.dictOfAPIs = dictionary as! [String : Dictionary<String, String>]
     }
 
 
