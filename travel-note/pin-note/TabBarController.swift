78fd280ef7a2f58252015cd8403c91548c5d4f08
Дарья <spanchdie@MacBook-Pro-Dara.local>
2020-11-23 17:33:15 +0300

    Новый ТАб Бар

--- travel-note/pin-note/TabBarController.swift
+++ travel-note/pin-note/TabBarController.swift
@@ -12,13 +12,21 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        //tabBar.backgroundColor = UIColor.init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.6)
+    
+        
         let mapVC = HomeViewController()
-        mapVC.tabBarItem = UITabBarItem(title: "Карта", image: UIImage.add, tag: 0) //STRINGS:
+        mapVC.tabBarItem = UITabBarItem(title: "Карта", image: UIImage(systemName: "map"), tag: 0) //STRINGS:
+        
         
         let listVC = ListNotesViewController()
-        listVC.tabBarItem = UITabBarItem(title: "Список", image: UIImage.actions, tag: 1) //STRINGS:
+        listVC.tabBarItem = UITabBarItem(title: "Список", image: UIImage(systemName: "doc.text"), tag: 1) //STRINGS:
+        
         
         self.viewControllers = [mapVC, listVC]
+        
+    
         
     }
     
