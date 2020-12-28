ffe4f4781bbf2ff36c669208dc6e189fb1739644
Alexey <lesh77906@gmail.com>
2020-12-29 06:55:04 +1100

    добавление отладочного функционала на страницу выбора темы (в последующем будет заменен на tableView)

---
+++ travel-note/pin-note/Settings/MainSection/ThemeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ThemeViewController.swift
+//  pin-note
+//
+//  Created by Алексей Савельев on 29.12.2020.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    private let theme = ThemeManager.currentTheme()
+    
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = .systemBackground
+        
+        // инит
+        let items = ["Gray", "Dust"]
+        let SC = UISegmentedControl(items: items)
+        SC.selectedSegmentIndex = Common.Settings.SelectedTheme
+        
+        // рамка
+        let frame = UIScreen.main.bounds
+        SC.frame = CGRect(x: frame.minX + 10, y: frame.minY + 25,
+                                width: frame.width - 30, height: frame.height*0.05)
+        
+        // углы и цвет
+        SC.layer.cornerRadius = 4.0
+        SC.backgroundColor = theme.barTintColor
+        SC.tintColor = theme.secondaryColor
+
+        // обработка изменения значения
+        SC.addTarget(self, action: #selector(sgDidSwich(sender:)), for: .valueChanged)
+
+        self.view.addSubview(SC)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = theme.secondaryColor
+    }
+    
+    // обработка изменения значения SC
+    @objc func sgDidSwich(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            Common.Settings.SelectedTheme = Int(sender.selectedSegmentIndex)
+            ThemeManager.applyTheme(theme: Theme(rawValue: Int(sender.selectedSegmentIndex))!)
+            self.view.layoutIfNeeded()
+        case 1:
+            Common.Settings.SelectedTheme = Int(sender.selectedSegmentIndex)
+            ThemeManager.applyTheme(theme: Theme(rawValue: Int(sender.selectedSegmentIndex))!)
+            self.view.layoutIfNeeded()
+        default:
+            print("gg")
+        }
+    }
+
+}
