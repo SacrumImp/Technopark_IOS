ebf045c437aa938e645b4b4b7d11de497018986c
Alexey <lesh77906@gmail.com>
2020-12-30 00:17:47 +1100

    Отображение коллекции списков

--- travel-note/pin-note/ListNotes/ListNotesViewController.swift
+++ travel-note/pin-note/ListNotes/ListNotesViewController.swift
@@ -7,73 +7,62 @@
 
 import UIKit
 
-class ListNotesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+class ListNotesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, NoteLayoutDelegate {
     
     var viewModel: ListNotesViewModelProtocol!
     
     var notesList = [Notes]()
+    
+    private var theme = ThemeManager.currentTheme()
     
     private let testSettingsButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 280, y: 80, width: 120, height: 35))
         button.setTitle("Настройки", for: .normal)
         button.setTitleColor(UIColor.red, for: .normal) //красным чтоб заметно было
         button.layer.backgroundColor = UIColor.systemRed.cgColor.copy(alpha: 0.3)
+        button.layer.borderWidth = 1
         button.layer.cornerRadius = 8
         return button
     }()
     
-    private let mainView: UIScrollView = {
-        let view = UIScrollView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .lightGray
-        return view
-    }()
-    
     private var notesCollection: UICollectionView = {
-        var width = UIScreen.main.bounds.size.width
-        let height = UIScreen.main.bounds.size.height
-        let marginLeft = CGFloat(15)
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        var collection = UICollectionView(frame: CGRect(x: marginLeft, y: 289 + (height/2), width: width - (marginLeft*2), height: 120), collectionViewLayout: layout)
-        let leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 5, height: 2.0))
-        collection.register(NoteCell.self, forCellWithReuseIdentifier: "noteCell")
-        collection.backgroundColor = .lightGray
-        collection.layer.cornerRadius = 5
-        collection.layer.borderColor = UIColor.lightGray.cgColor
-        collection.layer.borderWidth = 1.0
+        let layout = NoteLayout()
+        var collection = UICollectionView(frame: CGRect(x: 0, y: 150, width: UIScreen.main.bounds.size.width, height: (UIScreen.main.bounds.size.height - 150)), collectionViewLayout: layout)
         collection.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         return collection
     }()
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        ThemeManager.currentTheme()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        if let layout = notesCollection.collectionViewLayout as? NoteLayout {
+          layout.delegate = self
+        }
+        notesCollection.contentInset = UIEdgeInsets(top: 23, left: 16, bottom: 10, right: 16)
         
+        view.backgroundColor = theme.firstColor
         view.addSubview(testSettingsButton)
         testSettingsButton.addTarget(self, action: #selector(openSettings(sender:)), for: .touchUpInside)
+        testSettingsButton.setTitleColor(theme.barButtons, for: .normal) //красным чтоб заметно было
+        testSettingsButton.layer.backgroundColor = theme.thirdColor.cgColor.copy(alpha: 0.3)
+        testSettingsButton.layer.borderColor = theme.mainColor.cgColor
         
         notesList = viewModel.getNotes()
         
-        view.addSubview(mainView)
-        mainView.addSubview(notesCollection)
-        notesCollection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+        view.addSubview(notesCollection)
+        notesCollection.register(NoteCell.self, forCellWithReuseIdentifier: "noteCell")
         notesCollection.delegate = self
         notesCollection.dataSource = self
-        //mainView.contentSize = CGSize(width: UIScreen.main.bounds.size.width, height: mainView.bounds.size.height)
+        notesCollection.backgroundColor = theme.secondColor
         
-        configureConstraints()
-    }
-    
-    private func configureConstraints() {
-        var constraints = [NSLayoutConstraint]()
-        
-        constraints.append(mainView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
-        constraints.append(mainView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
-        constraints.append(mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor))
-        constraints.append(mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100))
-        
-        NSLayoutConstraint.activate(constraints)
+        notesCollection.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        notesCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        notesCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        notesCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
     
     @objc private func openSettings(sender: UIButton) {
@@ -84,9 +73,12 @@ class ListNotesViewController: UIViewController, UICollectionViewDataSource, UIC
         self.present(navVC, animated: true)
     }
     
+    
+    
     // MARK: collectionView delegate funcs
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: 100)
+        let itemSize = (collectionView.frame.width - (collectionView.contentInset.left + collectionView.contentInset.right + 10)) / 2
+        return CGSize(width: itemSize, height: itemSize/2)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -100,35 +92,70 @@ class ListNotesViewController: UIViewController, UICollectionViewDataSource, UIC
             print(notesList, indexPath.row)
             cell.photo.image = UIImage(data: notesList[indexPath.row].media)
             cell.titleLabel.text = notesList[indexPath.row].title
+            cell.infoLabel.text = notesList[indexPath.row].info
         }
         notesCollection.layer.borderColor = UIColor.gray.cgColor
+        cell.backgroundColor = theme.thirdColor
+        cell.layer.cornerRadius = 16
         return cell
     }
+    
+    // нажатие на ячейку коллекции
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? NoteCell
+        
+        // создание алерта
+        let alert = UIAlertController(title: "Удаление...", message: "Вы действительно хотите удалить заметку?", preferredStyle: UIAlertController.Style.actionSheet)
 
+        // действия алерта
+        alert.addAction(UIAlertAction(title: "Удалить", style: UIAlertAction.Style.destructive, handler: { action in
+            collectionView.deleteItems(at: [indexPath])
+            // TODO: удаление заметки из БД CoreData
+            
+        }))
+        // действия алерта
+        alert.addAction(UIAlertAction(title: "Редактировать", style: UIAlertAction.Style.default, handler: { action in
+            
+            var note = self.notesList[indexPath.row]
+            let EditVC = AddNoteViewController(currentNote: note)
+            let navVC = UINavigationController(rootViewController: EditVC)
+            navVC.modalTransitionStyle = .crossDissolve
+            navVC.modalPresentationStyle = .fullScreen
+            self.present(navVC, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Отмена", style: UIAlertAction.Style.cancel, handler: nil))
+
+        // вывод алерта
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat {
+      return (UIImage(data: notesList[indexPath.item].media)?.size.height)!
+    }
 }
 
 class NoteCell: UICollectionViewCell {
     fileprivate let photo: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 15
         return imageView
     }()
     
     let titleLabel: UILabel = {
-        let marginLeft = CGFloat(15)
-        let lable = UILabel(frame:CGRect(x: marginLeft, y: 20, width: 150, height: 25))
-        lable.font = .systemFont(ofSize: 15, weight: .bold)
+        let lable = UILabel()
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.font = .systemFont(ofSize: 12, weight: .bold)
         lable.textAlignment = .left
         return lable
     }()
     
     let infoLabel: UILabel = {
-        let marginLeft = CGFloat(15)
-        let lable = UILabel(frame:CGRect(x: marginLeft, y: 20, width: 150, height: 25))
-        lable.font = .systemFont(ofSize: 15, weight: .bold)
+        let lable = UILabel()
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.font = .systemFont(ofSize: 10, weight: .regular)
         lable.textAlignment = .left
         return lable
     }()
@@ -138,14 +165,24 @@ class NoteCell: UICollectionViewCell {
         contentView.addSubview(photo)
         contentView.addSubview(titleLabel)
         contentView.addSubview(infoLabel)
+        
         photo.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        photo.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-        photo.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: photo.bottomAnchor).isActive = true
+        photo.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        photo.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        photo.heightAnchor.constraint(equalToConstant: 157).isActive = true
+
+        titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 4).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: photo.bottomAnchor, constant: 10).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -4).isActive = true
+        
+
+        infoLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2).isActive = true
+        infoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 4).isActive = true
+        infoLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -4).isActive = true
+        infoLabel.heightAnchor.constraint(equalToConstant: 14).isActive = true
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
